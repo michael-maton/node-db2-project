59d78f2a94ccef44bba5adbefe3e6686ec56d518
Michael Maton <mjmaton6@gmail.com>
2021-01-19 15:07:16 -0600

    migrated to latest, seed created and seed data ran, everything connected and working

---
+++ data/seeds/01-cars.js
@@ -0,0 +1,11 @@
+
+exports.seed = function(knex) {
+  return knex('cars').truncate()
+    .then(function () {
+      return knex('cars').insert([
+        { VIN: '4Y1SL65806Z411439', make: "Nissan", model: "Sentra", mileage: 130500, transmission: "A", status: "Totaled" },
+        { VIN: '3A1SL45846N434437', make: "VW", model: "GTI", mileage: 34742, transmission: "M", status: "Mint" },
+        { VIN: '1D1SD45547V432549', make: "Toyota", model: "Highlander", mileage: 5685, transmission: "A", status: "New" }
+      ]);
+    });
+};
